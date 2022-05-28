b7521953f762af3d4430ef9ecc7ce1ee265831b7
alfonzomario <96298309+alfonzomario@users.noreply.github.com>
2022-05-28 17:35:50 -0300

    base de datos

--- api/src/models/diet.js
+++ api/src/models/diet.js
@@ -4,17 +4,8 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('diet', {
-    id:{
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
     name: {
       type: DataTypes.STRING,
     },
-    createdInDb: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },  
   });
 };
